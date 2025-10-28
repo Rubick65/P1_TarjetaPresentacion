b72db0d19c6bf8cad5bdba36d7fa294aa689c64d
Rubén Martín <martinandraderuben@gmail.com>
2025-10-28 18:15:15 +0100

    Configuración incial y logos añadidos

--- app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
+++ app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.p1_tarjetapresentacion_rubnmartn_hugodepablo.ui.theme.P1_TarjetaPresentacion_RubénMartín_HugoDePabloTheme
 
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             P1_TarjetaPresentacion_RubénMartín_HugoDePabloTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -31,17 +32,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContenedorUI(modifier: Modifier = Modifier){
+    val imagen = painterResource(R.drawable.fondo)
+    Box(modifier = modifier){
+        Image(
+            painter = imagen,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        // Aquí va la función del centro (Hugo De Pablo)
+
+        // Aquí va la función de abajo (Rubén Martín Andrade)
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true,
+    showSystemUi = true,
+    name = "My preview")
 @Composable
 fun GreetingPreview() {
     P1_TarjetaPresentacion_RubénMartín_HugoDePabloTheme {
-        Greeting("Android")
+        ContenedorUI(modifier = Modifier.fillMaxSize())
     }
 }

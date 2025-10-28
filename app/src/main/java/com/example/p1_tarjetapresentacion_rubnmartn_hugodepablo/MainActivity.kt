27fe89397af876a60f0b5ff0c57119b52bbd93fe
Rubén Martín <martinandraderuben@gmail.com>
2025-10-28 23:05:07 +0100

    Fusión de partes

--- app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
+++ app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,7 +26,6 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,9 +46,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ContenedorUI(modifier: Modifier = Modifier) {
-    val imagen = painterResource(R.drawable.gengar)
-    Box(modifier = modifier) {
+fun ContenedorUI(modifier: Modifier = Modifier){
+    val imagen = painterResource(R.drawable.fondo)
+    Box(modifier = modifier){
         Image(
             painter = imagen,
             contentDescription = null,
@@ -55,8 +58,46 @@ fun ContenedorUI(modifier: Modifier = Modifier) {
         // Aquí va la función del centro (Hugo De Pablo)
         ContenedorCentro()
         // Aquí va la función de abajo (Rubén Martín Andrade)
+        ContenedorContacto()
     }
 }
+
+@Composable
+private fun ContenedorContacto(modifier: Modifier = Modifier) {
+    val CANTIDAD_CONTACTOS = 4
+    val listaIconos = listOf<Int>(
+        R.drawable.phone_icono,
+        R.drawable.share_icono,
+        R.drawable.mail_icono,
+        R.drawable.github_logo
+    )
+
+    val listaTextos = listOf<Int>(
+        R.string.numero_inventado,
+        R.string.redes_inventadas,
+        R.string.email_inventado,
+        R.string.github_inventado
+    )
+
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(bottom = 40.dp)
+    ) {
+
+        for (i in 0 until CANTIDAD_CONTACTOS) {
+            FilasContacto(listaTextos[i], listaIconos[i])
+            Spacer(Modifier.height(10.dp))
+        }
+
+    }
+}
+
+
+
+
 
 @Composable
 fun CrearTexto(texto: String, modifier: Modifier = Modifier) {
@@ -109,11 +150,42 @@ fun ContenedorCentro(modifier: Modifier = Modifier) {
 }
 
 
+@Composable
+private fun FilasContacto(
+    textoID: Int,
+    imagenId: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier.padding(start = 50.dp, end = 40.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                painter = painterResource(imagenId),
+                contentDescription = null,
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(24.dp)
+            )
+            Spacer(Modifier.width(10.dp))
+
+            Text(
+                text = stringResource(textoID),
+                textAlign = TextAlign.Start,
+                color = Color.Black
+            )
+        }
+
+    }
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = true,
-    name = "My preview"
-)
+    name = "My preview")
 @Composable
 fun GreetingPreview() {
     P1_TarjetaPresentacion_RubénMartín_HugoDePabloTheme {

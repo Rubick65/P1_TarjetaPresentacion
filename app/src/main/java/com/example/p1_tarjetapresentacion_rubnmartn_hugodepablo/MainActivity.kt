a7dca0996ea79b4dc9f7002630778290cfef8d2b
Hugo de Pablo <hugdpalo@gmail.com>
2025-10-29 13:11:33 +0100

    Parte de Hugo de Pablo

--- app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
+++ app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.p1_tarjetapresentacion_rubnmartn_hugodepablo.ui.theme.P1_TarjetaPresentacion_RubénMartín_HugoDePabloTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             P1_TarjetaPresentacion_RubénMartín_HugoDePabloTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    ContenedorUI(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -32,9 +43,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ContenedorUI(modifier: Modifier = Modifier){
-    val imagen = painterResource(R.drawable.fondo)
-    Box(modifier = modifier){
+fun ContenedorUI(modifier: Modifier = Modifier) {
+    val imagen = painterResource(R.drawable.gengar)
+    Box(modifier = modifier) {
         Image(
             painter = imagen,
             contentDescription = null,
@@ -42,15 +53,67 @@ fun ContenedorUI(modifier: Modifier = Modifier){
             modifier = Modifier.fillMaxSize()
         )
         // Aquí va la función del centro (Hugo De Pablo)
-
+        ContenedorCentro()
         // Aquí va la función de abajo (Rubén Martín Andrade)
     }
 }
 
+@Composable
+fun CrearTexto(texto: String, modifier: Modifier = Modifier) {
+    Text(
+        text = texto,
+        fontSize = 50.sp,
+        color = Color.White,
+        lineHeight = 20.sp,
+        modifier = modifier
+    )
+}
 
-@Preview(showBackground = true,
+@Composable
+fun CrearImagen(imagen: Painter, modifier: Modifier = Modifier) {
+    Image(
+        painter = imagen,
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ContenedorCentro(modifier: Modifier = Modifier) {
+    val logoKotlin = painterResource(R.drawable.kotlin_logo)
+    val logoAndroid = painterResource(R.drawable.android_studio_logo)
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 10.dp)
+    ) {
+        Row() {
+            CrearImagen(logoKotlin,modifier.size(120.dp))
+            CrearTexto(stringResource(R.string.union), modifier.padding(top = 30.dp))
+            CrearImagen(logoAndroid, modifier.size(120.dp).padding(start = 15.dp))
+        }
+        CrearTexto(stringResource(R.string.nombre1))
+        CrearTexto(stringResource(R.string.enlace))
+        CrearTexto(stringResource(R.string.nombre2))
+        Text(
+            text = stringResource(R.string.titulo),
+            color = Color.Green,
+            fontSize = 18.sp,
+            modifier = modifier.padding(top = 10.dp)
+        )
+    }
+
+
+}
+
+
+@Preview(
+    showBackground = true,
     showSystemUi = true,
-    name = "My preview")
+    name = "My preview"
+)
 @Composable
 fun GreetingPreview() {
     P1_TarjetaPresentacion_RubénMartín_HugoDePabloTheme {

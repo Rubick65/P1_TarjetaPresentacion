f47a738040e98130a883f9a2ec41ec292ef73ce0
Rubén Martín <martinandraderuben@gmail.com>
2025-10-30 19:10:07 +0100

    Trabajo ahora si terminado ajuste de tamaño fijo de la columna de contactos v2

--- app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
+++ app/src/main/java/com/example/p1_tarjetapresentacion_rubnmartn_hugodepablo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -179,7 +180,8 @@ private fun FilasContacto(
 ) {
     // Fila que contiene el icono y el texto del contacto
     Row(
-        verticalAlignment = Alignment.CenterVertically // Centramos todos los elementos verticalmente
+        verticalAlignment = Alignment.CenterVertically, // Centramos todos los elementos verticalmente
+        modifier = Modifier.fillMaxWidth()
     ) {
         // Icono del contacto
         Icon(
